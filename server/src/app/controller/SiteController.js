dea4406d77d434d83d0777bc0ca5928b8aecd471
Quyền Vũ <vuquyen11a2k55@gmail.com>
2021-12-10 23:37:06 +0700

    edit validation

--- server/src/app/controller/SiteController.js
+++ server/src/app/controller/SiteController.js
@@ -1,22 +1,23 @@
-const path = require('path');
-const bcrypt = require('bcrypt');
+const path = require("path");
+const bcrypt = require("bcrypt");
 const dotenv = require("dotenv");
 
 dotenv.config();
-const User = require('../model/User');
+const User = require("../model/User");
 
-const {SESS_NAME, SALT_ROUND} = process.env;
-const {validationUsername, validationPassword} = require('../validation/UserValidation');
-
+const { SESS_NAME, SALT_ROUND } = process.env;
+const {
+  validationUsername,
+  validationPassword,
+} = require("../validation/UserValidation");
 
 class SiteController {
-
   home(req, res) {
-    res.sendFile(path.join(process.cwd(), 'src/dist', 'index.html'));
+    res.sendFile(path.join(process.cwd(), "src/dist", "index.html"));
   }
 
   async register(data) {
-    try { 
+    try {
       const hash = bcrypt.hashSync(data.password, parseInt(SALT_ROUND));
       await User.sync();
       return await User.create({
@@ -25,7 +26,7 @@ class SiteController {
         fullName: data.fullName,
         phone: data.phone,
         role: data.role,
-        group: data.group
+        group: data.group,
       });
     } catch {
       return null;
@@ -33,37 +34,51 @@ class SiteController {
   }
 
   async login(req, res) {
-    const {username, password} = req.body;
-    if (!await validationUsername(username, 'login')) {
+    const { username, password } = req.body;
+    if (!(await validationUsername(username, "login"))) {
       return res.json({ status: 0, error: "USERNAME_ERROR!" });
     }
-    const user = await User.findOne({
-      where: { username: username },
-    });
-    if (!validationPassword(password || !bcrypt.compareSync(password, user.password))) {
-      return res.json({ status: 0, error: "PASSWORD_ERROR!" });
+    try {
+      //
+      const user = await User.findOne({
+        where: { username: username },
+      });
+      //
+      if (
+        !validationPassword(password) ||
+        !bcrypt.compareSync(password, user.password)
+      ) {
+        return res.json({ status: 0, error: "PASSWORD_ERROR!" });
+      }
+      //Create session to user
+      req.session.username = username;
+      //
+      return res.json({ status: 1, group: user.group });
+    } catch (e) {
+      return res.json({ status: 0, error: "LOGIN_ERROR!" });
     }
-    req.session.username = username;
-    return res.json({ status: 1, group: user.group });
   }
 
   async logout(req, res) {
-    req.session.destroy(err => {
+    req.session.destroy((err) => {
+      //
       if (err) {
-        return res.json({status: 0});
+        return res.json({ status: 0 });
       }
+      //
       res.clearCookie(SESS_NAME);
-      res.json({status: 1});
-    })
+      //
+      res.json({ status: 1 });
+    });
   }
 
   async test(req, res) {
-    const date1 = new Date('abc');
+    const date1 = new Date("abc");
     const date2 = new Date();
-    date2.setTime(date2.getTime() + 24*60*60*1000);
-    console.log(date2, date1,date2 - date1);
-    res.json({status: 1});
+    date2.setTime(date2.getTime() + 24 * 60 * 60 * 1000);
+    console.log(date2, date1, date2 - date1);
+    res.json({ status: 1 });
   }
 }
 
-module.exports = new SiteController;
+module.exports = new SiteController();
